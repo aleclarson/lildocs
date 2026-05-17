8fb374655e3db9e71f8d031629f0e73cc05f2870
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-05-17 11:35:55 -0400

    chore: style docs preview

---
+++ docs/theme.ts
@@ -0,0 +1,17 @@
+// Generated from Shiki theme: GitHub Dark
+export default {
+  "color": {
+    "background": "#24292e",
+    "text": "#e1e4e8",
+    "mutedText": "#959da5",
+    "border": "#1b1f23",
+    "link": "#79b8ff",
+    "codeBackground": "#2f363d",
+    "sidebarBackground": "#1f2428"
+  },
+  "font": {
+    "heading": "system-ui, sans-serif",
+    "body": "system-ui, sans-serif",
+    "code": "ui-monospace, SFMono-Regular, Menlo, Consolas, monospace"
+  }
+} as const;
